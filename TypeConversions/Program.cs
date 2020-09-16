1e9abe788c5917b09007eed89ff68fa7278d5504
Vadim Potorocha <windrunner228@mail.ru>
2020-09-16 17:11:06 +0700

    ifelse :?

--- TypeConversions/Program.cs
+++ TypeConversions/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -30,10 +31,12 @@ namespace TypeConversions
 
 			//DeclareImplicitVars();
 
-			IterationCyclesPractice();
+			//IterationCyclesPractice();
+
+			IfElseExample();
 
 		}
-		
+
 		static void NarrowingAttemp()
 		{
 			byte myByte = 0;
@@ -66,9 +69,9 @@ namespace TypeConversions
 			{
 				Console.WriteLine(ex.Message);
 			}
-	
 
-			
+
+
 		}//checked unchecked проверка для суживающих преобразований данных и предотвращения или сокрытия переполнения переменной
 
 		static void DeclareImplicitVars() //Использование неявно типизированных переменных
@@ -126,10 +129,110 @@ namespace TypeConversions
 			*/
 
 			//Когда требуется повторять блок кода определенное количество раз, заранее известное, лучшим вариантом будет цикл FOR
+			//goto break continue может использоваться в любом виде цикла
 
+
+			for (int i = 0; i < 5; i++)
+			{
+				Console.WriteLine("For cycle there");
+			}
+
+			//foreach идеальный цикл для перебора массива данных
+			//foreach шагает n+1 и никак по другому
+			//List<T> может применятся внутри цикла foreach
+
+			string[] sportCars = { "Mazda", "Honda", "Subaru", "Suzuki", "Toyota", "Ford" };
+			foreach (string cars in sportCars)
+			{
+				Console.WriteLine(cars);
+			}
+
+			int[] engineNumbers = { 240, 320, 560, 400, 600, 720 };
+			foreach (int eNumbers in engineNumbers)
+			{
+				Console.WriteLine(eNumbers);
+			}
+
+			//Неявная типизация в foreach
+
+			int[] bibNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+			var subset = from i in bibNumbers where i < 10 select i;
+
+			Console.Write("Values in subset\n");
+
+			foreach (var i in bibNumbers)
+			{
+				Console.Write(" {0}\n", i);
+			}
+
+			//while цикл
+			//Повторяет цикл до того момента пока условие не будет выполнено
+
+			string userIsDone = "";
+			//проверка копии строки в нижнем регистре
+			while (userIsDone.ToLower() != "yes")
+			{
+				Console.WriteLine("We are in the while loop");
+				Console.WriteLine("Submit [yes] or [no] in the console to next iteration");
+				userIsDone = Console.ReadLine();
+			}
+
+			Console.WriteLine("Yor are done the loop");
+
+
+			//do while цикл - используется когда цикл должен выполниться неопределенное количество раз
+
+			string userIsDone2 = "";
+
+			do
+			{
+				Console.WriteLine("In do/while loop");
+				Console.WriteLine("Are you done [yes] [no]");
+				userIsDone2 = Console.ReadLine();
+			}
+
+			while (userIsDone2.ToUpper() != "NO");
+
+		}//4 вида циклов for foreach while (do while - для неопределенного количества раз)
+
+		static void IfElseExample()//If else может обрабатывать только Boolean значения, исключение - значение равное 0
+		{
+
+			string casualData = "My casual Data";
+			string casualDataCopy = "My casual DataCopy";
+
+			if (casualData.Length == casualDataCopy.Length)
+			{
+				Console.WriteLine("Hello!");
+			}
+
+			else
+			{
+				Console.WriteLine("casualData not Equal casualDataCopy");
+			}
+
+			//Условные операции ? :
+
+			string conditionalTest = "Asus";
+
+			Console.WriteLine(conditionalTest.Length >0
+				? "string is greater than 0 symbols"
+				: "string IS NOT greater than 0 symbols");
 
 
 		}
 
+
+
+
 	}
 }
+			
+
+
+
+
+
+
+
