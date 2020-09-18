7e6bd707c3ec5721f9ed89562bcc011fe695808f
Vadim Potorocha <windrunner228@mail.ru>
2020-09-18 20:37:21 +0300

    Switch case, c# 7.0 Switch case learning . If else done

--- TypeConversions/Program.cs
+++ TypeConversions/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TypeConversions
 {
@@ -33,7 +29,11 @@ namespace TypeConversions
 
 			//IterationCyclesPractice();
 
-			IfElseExample();
+			//IfElseExample(); Использование if else и логических операций && || !=
+
+			//SwitchCasePractice();
+
+			SwitchEnumPractice();
 
 		}
 
@@ -216,11 +216,253 @@ namespace TypeConversions
 
 			string conditionalTest = "Asus";
 
-			Console.WriteLine(conditionalTest.Length >0
+			Console.WriteLine(conditionalTest.Length > 0
 				? "string is greater than 0 symbols"
 				: "string IS NOT greater than 0 symbols");
 
 
+
+			string nameGirl = "Marina";
+
+			Console.WriteLine(nameGirl.Length < 0
+				? "string is not smaller than 0 symbols"
+				: "string is smaller than 0 symbols");
+
+			Console.WriteLine();
+
+
+			//Логические операции
+			/*
+			 * && возвращает true если все выражения дают true (имееи значение"И") 
+			 *
+			 * || возвращает true или false если одно из выражений даёт true или false (имеет значение "ИЛИ")
+			 *
+			 * ! возвращает false если выражение даёт true и наоборот (имеет значение "НЕ")
+			 *
+			 *
+			 */
+
+			string Sasha, Juja;
+			Sasha = "Alexandra";
+			Juja = "Jujolda";
+
+
+			//Сравнение двух строк через логические операции
+
+			if (Sasha != Juja)
+			{
+				Console.WriteLine("Alexandra");
+			}
+			else
+			{
+				Console.WriteLine("Jujolda");
+			}
+
+
+
+
+
+
+
+		}
+
+		static void SwitchCasePractice()
+		{
+
+			//Использование SWITCH CASE первый раз
+
+
+			//Console.WriteLine("1 [C#], 2 [VB]");
+			//Console.WriteLine("Please pick you language preference");
+
+			//string enteredMessage = Console.ReadLine();
+			//int enteredNumber = int.Parse(enteredMessage);
+
+			//switch (enteredMessage)
+			//{
+			//	case "1":
+			//		Console.WriteLine("C# is a good choice");
+			//		break;
+			//	case "2":
+			//		Console.WriteLine("VB is a good choice");
+			//		break;
+			//	default:
+			//		Console.WriteLine("Well good luck with {0}", enteredMessage);
+			//		break;
+			//}
+
+
+			//Выборка по мороженному в цикле, когда выбираешь одно и подтверждаешь, предлагает выбрать следующее
+
+
+			//	Console.WriteLine("Here is a few of ice creams");
+			//	Console.WriteLine("[Sundae], [Donduma], [Banana Split], [Sanwich], [Gelato]");
+
+			//Start:
+			//	string iceCream = Console.ReadLine();
+
+
+
+			//	while (iceCream == "Sundae" || iceCream == "Donduma" || iceCream == "Banana Split" || iceCream == "Sanwich" || iceCream == "Gelato")
+			//	{
+
+			//		switch (iceCream)
+			//		{
+			//			case "Sundae":
+			//				Console.WriteLine("Sundae is the most popular ice cream here, thank you");
+			//				break;
+			//			case "Donduma":
+			//				Console.WriteLine("Donudma is a great choice");
+			//				break;
+			//			case "Banana Split":
+			//				Console.WriteLine("Banana Split is so tasty");
+			//				break;
+			//			case "Sanwich":
+			//				Console.WriteLine("Sanwich like a SAND WITCH cmon");
+			//				break;
+			//			case "Gelato":
+			//				Console.WriteLine("Gelato is a good italion icecream very tasty and so tastyfull (hm ok)");
+			//				break;
+
+			//			default:
+			//				Console.WriteLine("You're better be thinking what u want to get");
+			//				break;
+			//		}
+
+			//		Console.WriteLine("\nChoose your ice cream");
+			//		goto Start;
+			//	}
+
+
+
+			//Автомат с подсчётом денег и забивом количества штук 
+
+			Console.WriteLine("Choose your item from the market");
+			Console.WriteLine(" 1: Tomato\n 2: Potato\n 3: Milk\n 4: Tea\n 5: Chocolate\n");
+
+			StartItem:
+			string selectedItem = Console.ReadLine();
+			int selectedItemNumber = int.Parse(selectedItem);
+
+			
+
+			// Счётчик денег
+			int cost = 0;
+			int finallCost = 0;
+
+			switch (selectedItemNumber)
+			{
+				case 1:
+					Console.WriteLine("You got a tomato. Nice!");
+					cost += 25;
+					break;
+
+				case 2:
+					Console.WriteLine("You got a potato. Nice!");
+					cost += 30;
+					break;
+
+				case 3:
+					Console.WriteLine("You got milk. Nice!");
+					cost += 35;
+					break;
+
+				case 4:
+					Console.WriteLine("You got a tea. Good!");
+					cost += 20;
+					break;
+
+				case 5:
+					Console.WriteLine("You got a chocolate. Very tasty!");
+					cost += 60;
+					break;
+
+				default:
+					Console.WriteLine("Try to enter number again to choose the item you really need");
+					goto StartItem;
+					break;
+			}
+
+			Console.WriteLine("Your full cost is {0}", cost);
+			Console.WriteLine("How many do you need of this? Maximum value is 5");
+
+			StartValue:
+			string valueOfItems = Console.ReadLine();
+			int numberOfItems = int.Parse(valueOfItems);
+
+			
+
+			switch (numberOfItems)
+			{
+				case 1:
+					finallCost = cost * numberOfItems;
+					Console.WriteLine("Your final cost of all items is {0}",finallCost);
+					break;
+
+				case 2:
+					finallCost = cost * numberOfItems;
+					Console.WriteLine("Your final cost of all items is {0}",finallCost);
+					break;
+
+				case 3:
+					finallCost = cost * numberOfItems;
+					Console.WriteLine("Your final cost of all items is {0}",finallCost);
+					break;
+
+				case 4:
+					finallCost = cost * numberOfItems;
+					Console.WriteLine("Your final cost of all items is {0}",finallCost);
+					break;
+
+				case 5:
+					finallCost = cost * numberOfItems;
+					Console.WriteLine("Your final cost of all items is {0}",finallCost);
+					break;
+
+
+				default:
+					Console.WriteLine("Enter a new number here");
+					goto StartValue;
+					break;
+			}
+
+
+			//
+
+
+
+		}
+
+		static void SwitchEnumPractice()
+		{
+			Console.WriteLine("Enter your favourite DayofWeek:");
+
+			DayOfWeek favday;
+
+			try
+			{
+				favday = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), Console.ReadLine());
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("Bad input");
+				return;
+			}
+
+			switch (favday)
+			{
+				case DayOfWeek.Monday:
+					Console.WriteLine("Another day another coding day");
+					break;
+
+				case DayOfWeek.Tuesday:
+					Console.WriteLine("Wake up and code on CSharp!");
+					break;
+
+				default:
+					Console.WriteLine("Cmon you need to code more in your life to get a good money and an awesome projects for more fun in life");
+					break;
+			}
 		}
 
 
@@ -228,7 +470,7 @@ namespace TypeConversions
 
 	}
 }
-			
+
 
 
 
